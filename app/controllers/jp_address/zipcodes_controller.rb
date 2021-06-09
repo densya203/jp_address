72f10b96323af893faa3bd24925406f293cbcdd0
densya203 <kamimori2@skult.jp>
2021-06-10 00:06:17 +0900

    バージョンを１に。

--- app/controllers/jp_address/zipcodes_controller.rb
+++ app/controllers/jp_address/zipcodes_controller.rb
@@ -3,7 +3,7 @@ require_dependency "jp_address/application_controller"
 module JpAddress
   class ZipcodesController < ApplicationController
     def search
-      @zipcode = Zipcode.find_by(:zip => params[:zip]) || Zipcode.new
+      @zipcode = Zipcode.find_by(:zip => params[:zip].to_s.gsub(/[^0-9]/, '')) || Zipcode.new
       render plain: @zipcode.to_json
     end
   end

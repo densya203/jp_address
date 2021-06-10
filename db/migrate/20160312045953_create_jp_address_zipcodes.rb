068482125c12380977d26902cd32f1ee562b0723
densya203 <kamimori2@skult.jp>
2021-06-10 09:45:53 +0900

    Rails 5 でも使用できるように、ActiveRecord::Migration のバージョン指定を 5.0 に。

--- db/migrate/20160312045953_create_jp_address_zipcodes.rb
+++ db/migrate/20160312045953_create_jp_address_zipcodes.rb
@@ -1,4 +1,4 @@
-class CreateJpAddressZipcodes < ActiveRecord::Migration[6.1]
+class CreateJpAddressZipcodes < ActiveRecord::Migration[5.0]
   def change
     create_table :jp_address_zipcodes do |t|
       t.string :zip       , null: false, index: true
